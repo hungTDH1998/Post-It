2179d30a70fcbfada71a1d181d91d971fda2a408
hungTDH1998 <49639660+hungTDH1998@users.noreply.github.com>
2023-06-22 22:49:22 +1000

    Sign out function completed

---
+++ app/auth/Logged.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import React from 'react'
+import Image from 'next/image'
+import { signOut } from 'next-auth/react'
+import Link from 'next/link'
+
+
+type User = {
+    image : String
+}
+
+export default function Logged({ image }: User) {
+  return (
+    <li className='flex gap-8'>
+        <button onClick={() => signOut()} className='bg-gray-700 text-sm text-white px-6 py-2 rounded-md'>
+            Sign out
+        </button>
+        <Link href={"/dashboard"}>
+            <Image width={64} 
+            height={64} 
+            src={image} 
+            className="w-10 rounded-full"
+            alt='' priority
+            />
+        </Link>
+    </li>
+  )
+}
--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
 import Login from './Login'
+import Logged from './Logged'
 import { authOptions } from '../../pages/api/auth/[...nextauth]'
 import { getServerSession } from "next-auth/next"
 
+
 export default async function Nav() {
     const session = await getServerSession(authOptions)
-    console.log(session)
     return (
     <nav className='flex justify-between items-center py-8'>
         <Link href={"/"}>
@@ -14,7 +15,7 @@ export default async function Nav() {
         </Link>
         <ul className='flex items-center py-8'>
             {!session?.user && <Login/>}
-            {session?.user && <h1>{session.user.name}</h1>}
+            {session?.user && <Logged image={session.user?.image || ""}/>}
         </ul>
     </nav>
   )
