c10045a3ef09e61ddbaeecb8d00cc6d1a4c837a0
hungTDH1998 <49639660+hungTDH1998@users.noreply.github.com>
2023-07-19 21:27:41 +1000

    add minor thing

--- pages/api/posts/deletePost.ts
+++ pages/api/posts/deletePost.ts
@@ -5,7 +5,7 @@ import prisma from "../../../prisma/client"
 
 
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest,res: NextApiResponse) {
   const session = await getServerSession(req, res, authOptions)
   if (!session) {
     return res.status(401).json({ message: "Please signin to create a post." })
